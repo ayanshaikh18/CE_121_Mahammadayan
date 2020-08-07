a80f9aeda19e97d111bb31ef6b372157948f88d9
ayanshaikh18 <mahammadayan18@gmail.com>
2020-08-07 11:26:03 +0530

    Lab 2 submitted

---
+++ LAB_2/addition.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace arithmetic
+{
+    public class addition
+    {
+        public static int sum(int x,int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ LAB_2/multiplication.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace arithmetic
+{
+    public class multiplication
+    {
+        public static int mul(int x,int y)
+        {
+            return x * y;
+        }
+    }
+}
